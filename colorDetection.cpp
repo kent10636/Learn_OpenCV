dfa06e8bd05174fed974ee8ced345b948d4c079b
Kent <767188747@qq.com>
2019-03-19 16:55:29 +0800

    floodFill

--- colorDetection.cpp
+++ colorDetection.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+	// ---------- 搜索并替换颜色 ----------
 	// 1. 创建图像处理器对象
 	ColorDetector cdetect;
 
@@ -25,6 +26,20 @@ int main()
 	cv::namedWindow("Result");
 	cv::Mat result = cdetect.process(image);
 	cv::imshow("Result", result);
+
+	// ---------- floodFill函数 ----------
+	// 找出与指定颜色接近的颜色（连续区域）
+	cv::floodFill(image,            // 输入/输出图像
+		cv::Point(100, 50),         // 起始点
+		cv::Scalar(255, 255, 255),  // 填充颜色
+		(cv::Rect*)0,               // 填充区域的边界矩形
+		cv::Scalar(35, 35, 35),     // 偏差的最小/最大阈值
+		cv::Scalar(35, 35, 35),     // 正差阈值，两个阈值通常相等
+		cv::FLOODFILL_FIXED_RANGE); // 与起始点像素比较
+
+	cv::namedWindow("floodFill Result");
+	result = cdetect.process(image);
+	cv::imshow("floodFill Result", image);
 
 	cv::waitKey();
 	return 0;
